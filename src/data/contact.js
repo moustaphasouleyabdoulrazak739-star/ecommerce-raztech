2618fe0ee2031685226ca801d64add8f1cb62235
moustaphasouleyabdoulrazak739-star <moustaphasouleyabdoulrazak739@gmail.com>
2026-08-02 19:13:35 +0100

    Ajout infos boutique dans le Footer (telephone, adresse, horaires)

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,7 @@
 // src/data/contact.js
 
 export const contact = {
-  telephones: ["+227 94 24 15 70", "+227 99 01 50 23"],
+  telephones: ["+227 98 19 89 25", "+227 99 01 50 23"],
   adresse: "Harobanda, Université Abdou Moumouni, en face de l'AGRHYMET, Niamey",
   horaires: [
     { jours: "Tous les jours", heures: "07h00 - 01h00" },
